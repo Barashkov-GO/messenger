d0ace890714e36b1872fc4a8fe591950a33e4333
DionisSerbin <reaganusm@gmail.com>
2022-09-16 18:52:51 +0300

    Добавил гугл аунтификацию

---
+++ lib/services/auth.dart
@@ -0,0 +1,67 @@
+// @dart=2.9
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:my_first_flutter/helperfunctions/sharedpref_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthMethods{
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  getCurrenctUser(){
+    return auth.currentUser;
+  }
+
+  signInWithGoogle(BuildContext context) async {
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount =
+        await _googleSignIn.signIn();
+
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount?.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken:  googleSignInAuthentication?.idToken,
+      accessToken: googleSignInAuthentication?.accessToken
+    );
+
+    UserCredential result =
+        await _firebaseAuth.signInWithCredential(credential);
+
+    User userDetails = result.user;
+
+    if (result == null) {
+    } else {
+      SharedPreferenceHelper().saveUserEmail(userDetails.email);
+      SharedPreferenceHelper().saveUserId(userDetails.uid);
+      SharedPreferenceHelper()
+          .saveUserName(userDetails.email.replaceAll("@gmail.com", ""));
+      SharedPreferenceHelper().saveDisplayName(userDetails.displayName);
+      SharedPreferenceHelper().saveUserProfileUrl(userDetails.photoURL);
+
+      DatabaseMethods()
+          .addUserInfoToDB(
+          userID: userDetails.uid,
+          email: userDetails.email,
+          username: userDetails.email.replaceAll("@gmail.com", ""),
+          name: userDetails.displayName,
+          profileUrl: userDetails.photoURL)
+          .then(() {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Home()));
+      });
+    }
+  }
+
+  Future signOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+    await auth.signOut();
+  }
+  }
+
+
+}
